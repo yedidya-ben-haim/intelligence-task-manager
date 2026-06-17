23356690f48b55a224810972c1beaa18c1b5ad2c
yedidya ben haim <bhyedidya@gmail.com>
2026-06-17 14:55:02 +0300

    Adding try and except to functions

--- database/db_connection.py
+++ database/db_connection.py
@@ -2,6 +2,9 @@ import mysql.connector
 
 
 class ConnectionDB:
+    """
+        Responsible for connecting to the MySQL container, creating the database, and creating the tables.
+    """
     def __init__(self):
         self.user = 'root'
         self.password = '1234'
@@ -9,8 +12,40 @@ class ConnectionDB:
         self.database = 'Intelligence_db'
 
     def get_connection(self):
-        return mysql.connector.connect(user=self.user, password=self.password,
+        """
+            Returns an active connection to MySQL
+        """
+        try:
+            return mysql.connector.connect(user=self.user, password=self.password,
                               host=self.host,
                               database=self.database)
+        except Exception as e:
+            return None
+
+
+    def create_database(self):
+        """
+            Creates Intelligence_db if it does not exist
+        """
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        query = """CREATE DATABASE IF NOT EXISTS Intelligence_db"""
+
+        try:
+            cursor.execute(query)
+
+            cursor.close()
+            conn.close()
+
+        except Exception as e:
+            return None
+
+
+
+new_db = ConnectionDB()
+new_db.create_database()
+
+
 
 
